62e2886bbf4912cf5ad128aca75262f65fb905b6
maciej <dupa1@poczta.dobra.pl>
2025-03-24 10:40:24 +0100

    Changed functioning of a button

--- app/src/main/java/com/example/kk/MainActivity.kt
+++ app/src/main/java/com/example/kk/MainActivity.kt
@@ -2,10 +2,24 @@
 package com.example.kk
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.Console
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +31,57 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val Button: Button = findViewById(R.id.Button)
+
+        val numberOf = findViewById<TextView>(R.id.NumberOf)
+        val myImageView: ImageView = findViewById(R.id.ImageView)
+        myImageView.setImageResource(R.drawable.kawa)
+        val radioGroup = findViewById<RadioGroup>(R.id.ButtonGroup)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId)
+            val caffeType = radioButton.text
+            Log.i("type", "$caffeType")
+            when (caffeType) {
+                "Espresso" -> myImageView.setImageResource(R.drawable.espresso);
+                "Capuccino" -> myImageView.setImageResource(R.drawable.capuccino);
+                "Latte" -> myImageView.setImageResource(R.drawable.latte);
+            }
+        }
+
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                numberOf.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+        Button.setOnClickListener {
+            val radioGroup = findViewById<RadioGroup>(R.id.ButtonGroup)
+            radioGroup.setOnCheckedChangeListener { _, checkedId ->
+                val radioButton = findViewById<RadioButton>(checkedId)
+                val caffeType = radioButton.text.toString()}
+
+                val checkBox = findViewById<CheckBox>(R.id.checkBoxGroup)
+                var checkBoxChecked = true
+                var checkBoxListener = checkBox.setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        checkBoxChecked = true
+                    } else {
+                        checkBoxChecked = false
+                    }
+                }.toString()
+
+                val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+                Log.d("Dupa","Zamówienie Rodzaj: , Dodatki: $checkBoxListener Ilość: ${seekBar.progress.toString()}")
+            }
+        }
     }
-}
